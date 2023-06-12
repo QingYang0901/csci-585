fa308476fef61c004f31f2628c9c084996cb8306
QingYang0901 <112041988+QingYang0901@users.noreply.github.com>
2023-06-11 21:45:46 -0700

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -12,26 +12,26 @@ CREATE TABLE classes{
   PRIMARY KEY (subject)
 };
 
-INSERT INTO instruction VALUES('Aleph',40);
-INSERT INTO instruction VALUES('Bit',40);
-INSERT INTO instruction VALUES('CRC',50);
-INSERT INTO instruction VALUES('Dat',60);
+INSERT INTO Instruction VALUES('Aleph',40);
+INSERT INTO Instruction VALUES('Bit',40);
+INSERT INTO Instruction VALUES('CRC',50);
+INSERT INTO Instruction VALUES('Dat',60);
 
-INSERT INTO classes VALUEs(10,'MIDI controllers','Aleph');
-INSERT INTO classes VALUEs(20,'Sound mixing','Aleph');
-INSERT INTO classes VALUEs(30,'Synthesis algorithms','Aleph');
-INSERT INTO classes VALUEs(20,'EDM synthesis','Bit');
-INSERT INTO classes VALUEs(30,'Electronic Music Fundamentals','Bit');
-INSERT INTO classes VALUEs(20,'Sound mixing','Bit');
-INSERT INTO classes VALUEs(30,'EDM synthesis','CRC');
-INSERT INTO classes VALUEs(20,'Electronic Music Fundamentals','CRC');
-INSERT INTO classes VALUEs(40,'MIDI controllers','Dat');
-INSERT INTO classes VALUEs(30,'EDM synthesis','Dat');
-INSERT INTO classes VALUEs(35,'Electronic Music Fundamentals','Dat');
+INSERT INTO Classes VALUES(10,'MIDI controllers','Aleph');
+INSERT INTO Classes VALUES(20,'Sound mixing','Aleph');
+INSERT INTO Classes VALUES(30,'Synthesis algorithms','Aleph');
+INSERT INTO Classes VALUES(20,'EDM synthesis','Bit');
+INSERT INTO Classes VALUES(30,'Electronic Music Fundamentals','Bit');
+INSERT INTO Classes VALUES(20,'Sound mixing','Bit');
+INSERT INTO Classes VALUES(30,'EDM synthesis','CRC');
+INSERT INTO Classes VALUES(20,'Electronic Music Fundamentals','CRC');
+INSERT INTO Classes VALUES(40,'MIDI controllers','Dat');
+INSERT INTO Classes VALUES(30,'EDM synthesis','Dat');
+INSERT INTO Classes VALUES(35,'Electronic Music Fundamentals','Dat');
 
-SELECT name, SUM(hourly_rate*srudent_count*o.1) AS bonus
-FROM instructors
-JOIN classes ON instructors.instructor_id=classes.instructor_id
-GROUP BY instructors.instructor_id
+SELECT Instructor, SUM(hourly_rate*srudent_count*o.1) AS bonus
+FROM Instruction
+JOIN Classes ON Instruction.Instructor=Classes.Instructor
+GROUP BY Instruction.instructor
 ORDER BY bonus DESC
 LIMIT 1;
