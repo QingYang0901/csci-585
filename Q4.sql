b05749caff6c36b206a00c42f90101448c282cd1
QingYang0901 <112041988+QingYang0901@users.noreply.github.com>
2023-06-11 21:42:20 -0700

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,24 +1,33 @@
 /* use livesql */
-CREATE TABLE instructors{
-instructor_id INTEGER PRIMARY KEY,
-hourly_rate FLOAT NOT NULL,
-name TEXT NOT NULL
-};
+CREATE TABLE Instruction(
+   Instructor CHAR(15),
+   hourly_rate INTEGER NOT NULL,
+   PRIMARY KEY (Instructor)
+   };
 
 CREATE TABLE classes{
-class_id INTEGER PRIMARY KEY,
-instructor_id INTEGER REFERENCES instructor(instructor_id);
-student_count INTEGER NOT NULL,
-subject TEXT NOT NULL
+  student_count INTEGER NOT NULL,
+  subject CHAR(30),
+  Instructor CHAR(15),
+  PRIMARY KEY (subject)
 };
 
-INSERT INTO instructor (instructor_id, hourly_rate, name)
-VALUES(1,40, 'Aleph'),(2,40,'Bit'),(3,50,'CRC'),(4,60,'Dat');
+INSERT INTO instruction VALUES('Aleph',40);
+INSERT INTO instruction VALUES('Bit',40);
+INSERT INTO instruction VALUES('CRC',50);
+INSERT INTO instruction VALUES('Dat',60);
 
-INSERT INTO classes (class_id, instructor_id, student_count, subject)
-VALUE(1,1,10,'MIDI controllers'),(2,1,20,'Sound mixing'),(3,1,30,'Synthesis algorithms'),(4,2,20,'EDM synthesis'),
-(5,2,30,'Electronic Music Fundamentals'),(2,2,20,'Sound mixing'),(4,3,30,'EDM synthesis'),(5,3,20,'Electronic Music Fundamentals'),
-(1,4,40,'MIDI controllers'),(4,4,30,'EDM synthesis'),(5,4,35,'Electronic Music Fundamentals');
+INSERT INTO classes VALUEs(10,'MIDI controllers','Aleph');
+INSERT INTO classes VALUEs(20,'Sound mixing','Aleph');
+INSERT INTO classes VALUEs(30,'Synthesis algorithms','Aleph');
+INSERT INTO classes VALUEs(20,'EDM synthesis','Bit');
+INSERT INTO classes VALUEs(30,'Electronic Music Fundamentals','Bit');
+INSERT INTO classes VALUEs(20,'Sound mixing','Bit');
+INSERT INTO classes VALUEs(30,'EDM synthesis','CRC');
+INSERT INTO classes VALUEs(20,'Electronic Music Fundamentals','CRC');
+INSERT INTO classes VALUEs(40,'MIDI controllers','Dat');
+INSERT INTO classes VALUEs(30,'EDM synthesis','Dat');
+INSERT INTO classes VALUEs(35,'Electronic Music Fundamentals','Dat');
 
 SELECT name, SUM(hourly_rate*srudent_count*o.1) AS bonus
 FROM instructors
