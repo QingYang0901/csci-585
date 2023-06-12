987bd4338a93b8fcca569770abd190eec8c34762
QingYang0901 <112041988+QingYang0901@users.noreply.github.com>
2023-06-11 23:45:44 -0700

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,37 +1,33 @@
 /* use livesql */
-CREATE TABLE Instruction(
-   Instructor CHAR(15),
-   hourly_rate INTEGER NOT NULL,
-   PRIMARY KEY (Instructor)
-   );
-
-INSERT INTO Instruction VALUES('Aleph',40);
-INSERT INTO Instruction VALUES('Bit',40);
-INSERT INTO Instruction VALUES('CRC',50);
-INSERT INTO Instruction VALUES('Dat',60);
-
-CREATE TABLE Classes(
-  student_count INTEGER NOT NULL,
-  subject CHAR(30),
-  Instructor CHAR(15),
-  PRIMARY KEY (subject)
+CREATE TABLE Instructors (
+  InstructorID INT PRIMARY KEY,
+  InstructorName VARCHAR(50),
+  HourlyRate DECIMAL(8, 2)
 );
-   
-INSERT INTO Classes VALUES(10,'MIDI controllers','Aleph');
-INSERT INTO Classes VALUES(20,'Sound mixing','Aleph');
-INSERT INTO Classes VALUES(30,'Synthesis algorithms','Aleph');
-INSERT INTO Classes VALUES(20,'EDM synthesis','Bit');
-INSERT INTO Classes VALUES(30,'Electronic Music Fundamentals','Bit');
-INSERT INTO Classes VALUES(20,'Sound mixing','Bit');
-INSERT INTO Classes VALUES(30,'EDM synthesis','CRC');
-INSERT INTO Classes VALUES(20,'Electronic Music Fundamentals','CRC');
-INSERT INTO Classes VALUES(40,'MIDI controllers','Dat');
-INSERT INTO Classes VALUES(30,'EDM synthesis','Dat');
-INSERT INTO Classes VALUES(35,'Electronic Music Fundamentals','Dat');
+CREATE TABLE Courses (
+  CourseID INT PRIMARY KEY,
+  CourseName VARCHAR(50),
+  InstructorID INT,
+  StudentCount INT,
+  FOREIGN KEY (InstructorID) REFERENCES Instructors(InstructorID)
+);
 
-SELECT Instructor, SUM(hourly_rate*srudent_count*0.1) AS bonus
-FROM Instruction
-JOIN Classes ON Instruction.Instructor=Classes.Instructor
-GROUP BY Instruction.instructor
-ORDER BY bonus DESC
-LIMIT 1;
+INSERT INTO Instructors VALUES (1, 'Aleph', 30.0);
+INSERT INTO Instructors VALUES(2, 'Bit', 40.0);
+INSERT INTO Instructors VALUES(3, 'CRC', 50.0);
+
+
+INSERT INTO Courses VALUES(1, 'csci 585', 1, 20);
+INSERT INTO Courses VALUES(2, 'csci 570', 1, 30);
+INSERT INTO Courses VALUES(3, 'ee 503', 2, 40);
+INSERT INTO Courses VALUES(4, 'ee 510', 3, 30);
+
+SELECT
+  i.InstructorName,
+  SUM(i.HourlyRate*c.StudentCount) * 0.1 AS Bonus
+FROM
+  Instructors i
+JOIN Courses c ON i.InstructorID = c.InstructorID 
+GROUP BY
+  i.InstructorName
+ORDER BY Bonus DESC FETCH FIRST ROW ONLY;
